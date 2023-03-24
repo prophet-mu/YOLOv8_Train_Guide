e11fbdd5c22bbdfc5b1b2c4ba630bb550de5aa5d
prophet-mu <96509826+prophet-mu@users.noreply.github.com>
2023-03-24 18:09:46 +0800

    Add files via upload

---
+++ split.py
@@ -0,0 +1,85 @@
+# 将图片和标注数据按比例切分为 训练集和测试集
+import shutil
+import random
+import os
+# 数据集路径
+image_original_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/images/'
+label_original_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/labels/'
+# 训练集路径
+train_image_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/train/images/'
+train_label_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/train/labels/'
+# 验证集路径
+val_image_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/val/images/'
+val_label_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/val/labels/'
+# 测试集路径
+test_image_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/test/images/'
+test_label_path = '/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/test/labels/'
+
+# 数据集划分比例，训练集75%，验证集15%，测试集15%，按需修改
+train_percent = 0.7
+val_percent = 0.15
+test_percent = 0.1
+
+
+# 检查文件夹是否存在
+def mkdir():
+    if not os.path.exists(train_image_path):
+        os.makedirs(train_image_path)
+    if not os.path.exists(train_label_path):
+        os.makedirs(train_label_path)
+
+    if not os.path.exists(val_image_path):
+        os.makedirs(val_image_path)
+    if not os.path.exists(val_label_path):
+        os.makedirs(val_label_path)
+
+    if not os.path.exists(test_image_path):
+        os.makedirs(test_image_path)
+    if not os.path.exists(test_label_path):
+        os.makedirs(test_label_path)
+
+
+def main():
+    mkdir()
+
+    total_txt = os.listdir(label_original_path)
+    num_txt = len(total_txt)
+    list_all_txt = range(num_txt)  # 范围 range(0, num)
+
+    num_train = int(num_txt * train_percent)
+    num_val = int(num_txt * val_percent)
+    num_test = num_txt - num_train - num_val
+
+    train = random.sample(list_all_txt, num_train)
+    # 在全部数据集中取出train
+    val_test = [i for i in list_all_txt if not i in train]
+    # 再从val_test取出num_val个元素，val_test剩下的元素就是test
+    val = random.sample(val_test, num_val)
+
+    print("训练集数目：{}, 验证集数目：{},测试集数目：{}".format(len(train), len(val), len(val_test) - len(val)))
+    for i in list_all_txt:
+        name = total_txt[i][:-4]
+
+        srcImage = image_original_path + name + '.jpg'
+        srcLabel = label_original_path + name + '.txt'
+
+        if i in train:
+            dst_train_Image = train_image_path + name + '.jpg'
+            dst_train_Label = train_label_path + name + '.txt'
+            shutil.copyfile(srcImage, dst_train_Image)
+            shutil.copyfile(srcLabel, dst_train_Label)
+        elif i in val:
+            dst_val_Image = val_image_path + name + '.jpg'
+            dst_val_Label = val_label_path + name + '.txt'
+            shutil.copyfile(srcImage, dst_val_Image)
+            shutil.copyfile(srcLabel, dst_val_Label)
+        else:
+            dst_test_Image = test_image_path + name + '.jpg'
+            dst_test_Label = test_label_path + name + '.txt'
+            shutil.copyfile(srcImage, dst_test_Image)
+            shutil.copyfile(srcLabel, dst_test_Label)
+
+if __name__ == '__main__':
+	main()
+
+
---
+++ xml_to_txt.py
@@ -0,0 +1,55 @@
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from os import listdir, getcwd
+from os.path import join
+
+
+def convert(size, box):
+    x_center = (box[0] + box[1]) / 2.0
+    y_center = (box[2] + box[3]) / 2.0
+    x = x_center / size[0]
+    y = y_center / size[1]
+    w = (box[1] - box[0]) / size[0]
+    h = (box[3] - box[2]) / size[1]
+    return (x, y, w, h)
+
+
+def convert_format(xml_files_path, save_txt_files_path, classes):
+    xml_files = os.listdir(xml_files_path)
+    print(xml_files)
+    for xml_name in xml_files:
+        print(xml_name)
+        xml_file = os.path.join(xml_files_path, xml_name)
+        out_txt_path = os.path.join(save_txt_files_path, xml_name.split('.')[0] + '.txt')
+        out_txt_f = open(out_txt_path, 'w')
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        size = root.find('size')
+        w = int(size.find('width').text)
+        h = int(size.find('height').text)
+
+        for obj in root.iter('object'):
+            difficult = obj.find('difficult').text
+            cls = obj.find('name').text
+            if cls not in classes or int(difficult) == 1:
+                continue
+            cls_id = classes.index(cls)
+            xmlbox = obj.find('bndbox')
+            b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+                 float(xmlbox.find('ymax').text))
+            # b=(xmin, xmax, ymin, ymax)
+            print(w, h, b)
+            bb = convert((w, h), b)
+            out_txt_f.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+
+if __name__ == "__main__":
+    # 需要转换的类别，需要一一对应
+    classes = ['rebar']
+    # 2、voc格式的xml标签文件路径
+    xml_files = r'/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/Annotations/'
+    # 3、转化为yolo格式的txt标签文件存储路径
+    save_txt_files = r'/home/prophetmu/m3axpi_model/ultralytics/ultralytics/datasets/rebar/label'
+
+    convert_format(xml_files, save_txt_files, classes)
